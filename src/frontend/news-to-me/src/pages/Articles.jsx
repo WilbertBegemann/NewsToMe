79b5cc39c564eabad03afca92a3a6e60a3919e5b
Begemann, CW, Mr [24791334@sun.ac.za] <24791334@sun.ac.za>
2024-01-10 13:05:23 +0200

    added functionality to go to original article

--- src/frontend/news-to-me/src/pages/Articles.jsx
+++ src/frontend/news-to-me/src/pages/Articles.jsx
@@ -1,12 +1,19 @@
 import React ,{useEffect, useState} from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import altImage from '../imgs/altimg.png'
 
 
 function ArticlesPage() {
     const [data, setData] = useState([{}]);
     const location = useLocation();
     const title = location.state && location.state.title;
+    const navigate = useNavigate();
+
+    const handleListItemClick = (link) => {
+        console.log(link);
+        window.open(link, '_blank');
+    };
 
     useEffect(() => {
         console.log(`http://127.0.0.1:5000/api/${title}`);
@@ -18,13 +25,14 @@ function ArticlesPage() {
         <div className='justify-center flex bg-slate-600 w-screen h-max'>
             <ul className='justify-center text-white font-bold text-xl text-left'>
                 {data.map((item, index) => (
-                    <li className='text-left' key={index} onClick={() => handleListItemClick(item.title)}>
+                    <li className='text-left' href={item.link}  key={index} onClick={() => handleListItemClick(item.link)}>
                     <div className='flex bg-slate-500 border-r-8 p-10 m-2'>
                         <div className='left-0'>
                         <img
                             src={item.image}
-                            alt=''
+                            alt={altImage}
                             style={{ width: '100%', height: '100%', objectFit: 'cover', flexShrink: 0 }}
+                            href={item.image}
                         />
                         </div>
                         <div className='left-0 p-6'>
@@ -34,7 +42,6 @@ function ArticlesPage() {
                     </li>
                 ))}
             </ul>
-
         </div>
     );
 };
